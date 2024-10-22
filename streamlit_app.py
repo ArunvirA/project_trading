c96acf2fa7fd2c31ae454b73fff4234b804c1ddb
Arunvir <arunviratwal@gmail.com>
2024-10-22 11:01:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from pmdarima import auto_arima
 from darts.models import Theta
 from darts import TimeSeries
 from sklearn.metrics import mean_absolute_error, mean_squared_error
