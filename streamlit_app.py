460b736cfcb1af59f52506d99fa61c1c32bb2f1d
Arunvir <arunviratwal@gmail.com>
2024-10-22 17:21:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,11 +76,14 @@ st.markdown(f"<p style='color: white; font-size: 18px;'>RMSE: {rmse_theta:.5f}</
 
 
 
-z_data = pd.read_csv('eurusd_24h.csv')
-z = z_data.values
-sh_0, sh_1 = z.shape
-x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
-  return x, y, z
+@st.cache
+def get_plotly_data():
+
+    z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+    z = z_data.values
+    sh_0, sh_1 = z.shape
+    x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
+    return x, y, z
 
 import plotly.graph_objects as go
 
@@ -88,8 +91,7 @@ x, y, z = get_plotly_data()
 
 fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
 fig.update_layout(title='IRR', autosize=False, width=800, height=800, margin=dict(l=40, r=40, b=40, t=40))
-go.plotly_chart(fig)
-
+st.plotly_chart(fig)
 
 
 
