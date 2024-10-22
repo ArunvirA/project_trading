a4a1c520f69939d195bff5f2721f808e0efc05cc
Arunvir <arunviratwal@gmail.com>
2024-10-22 12:05:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ theta = Theta()
 theta.fit(train_ts)
 forecast_values = theta.predict(len(test_ts))
 
-st.write("Plotting forecast results")
+st.subheader("Plotting forecast results")
 fig, ax = plt.subplots(figsize=(12, 6))
 train_ts.plot(label='Train', lw=2, ax=ax)
 test_ts.plot(label='Test', lw=2, ax=ax)
