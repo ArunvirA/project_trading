6f41b8866e15760bf78924c632f39d45d4811947
Arunvir <arunviratwal@gmail.com>
2024-10-22 10:44:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from pmdarima import auto_arima
+from darts.models import Theta
+from darts import TimeSeries
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 st.title("Data Led Trading: Predict, Profit, Prevail")
