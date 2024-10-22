bcf5462de3bbbed0f527be1d0d426949214b2f50
Arunvir <arunviratwal@gmail.com>
2024-10-22 14:32:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,12 @@ st.markdown("""This app forecasts EUR/USD close prices using machine learning sp
 df = pd.read_csv('eurusd_24h.csv')
 st.dataframe(df.head())
 
+df['Date_date'] = pd.to_datetime(df['Date_date'])
 df = df.sort_values(by='Date_date', ascending=True)
-df.set_index('Date_date', inplace=True, drop=False)
+df.set_index('Date_date', inplace=True)
+
+# df = df.sort_values(by='Date_date', ascending=True)
+# df.set_index('Date_date', inplace=True, drop=False)
 df = df[['Date_date', 'open', 'high', 'low', 'close', 'vol']]
 
 df['close'] = df['close'].interpolate()
@@ -20,11 +24,16 @@ df['close'] = df['close'].interpolate()
 # train, test = y[:int(len(y)*0.8)], y[int(len(y)*0.8):]
 
 # y2 = y
-full_date_range = pd.date_range(start=df['Date_date'].min(), end=df['Date_date'].max(), freq='D')
+# full_date_range = pd.date_range(start=df['Date_date'].min(), end=df['Date_date'].max(), freq='D')
+full_date_range = pd.date_range(start=df.index.min(), end=df.index.max(), freq='D')
 df_full = df.reindex(full_date_range)
-y_full = df_full['close'].fillna(method='ffill')
+df_full['close'] = df_full['close'].fillna(method='ffill')
+# y_full = df_full['close'].fillna(method='ffill')
 
-y_ts = TimeSeries.from_series(y_full, fill_missing_dates=True, freq="D")
+df_full.reset_index(inplace=True)
+df_full.rename(columns={'index': 'Date_date'}, inplace=True)
+
+y_ts = TimeSeries.from_series(df_full['close'], fill_missing_dates=True, freq="D")
 train_ts, test_ts = y_ts[:int(len(y_ts)*0.8)], y_ts[int(len(y_ts)*0.8):]
 
 st.write(df.reindex(full_date_range))
@@ -36,15 +45,27 @@ theta = Theta()
 theta.fit(train_ts)
 forecast_values = theta.predict(len(test_ts))
 
-st.write(forecast_values)
+st.subheader("Forecast Values")
+st.write(forecast_values.pd_dataframe())
 
-st.subheader("Plotting forecast results")
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(train_ts.values(),label='Train', lw=2)
+# st.write(forecast_values)
+
+# st.subheader("Plotting forecast results")
+# fig, ax = plt.subplots(figsize=(12, 6))
+# ax.plot(train_ts.values(),label='Train', lw=2)
 # ax.plot(test_ts.values(),label='Test', lw=2)
 # ax.plot(forecast_values.values(),label='Forecast', lw=2)
+# plt.xlabel('Date')
+# plt.title('EUR/USD Close Price Forecast using Theta')
+
+st.subheader("Plotting Forecast Results")
+fig, ax = plt.subplots(figsize=(12, 6))
+train_ts.plot(ax=ax, label='Train', lw=2)
+test_ts.plot(ax=ax, label='Test', lw=2)
+forecast_values.plot(ax=ax, label='Forecast', lw=2)
 plt.xlabel('Date')
 plt.title('EUR/USD Close Price Forecast using Theta')
+plt.legend()
 
 st.pyplot(fig)
 
