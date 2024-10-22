b4305da6dbe9b7e118ffc69d2cf53a63b5fe189b
Arunvir <arunviratwal@gmail.com>
2024-10-22 11:27:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from darts.models import Theta
 from darts import TimeSeries
 from sklearn.metrics import mean_absolute_error, mean_squared_error
