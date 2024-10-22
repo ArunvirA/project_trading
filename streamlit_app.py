419ac5006074c5a8c36d415898f171752f470c25
Arunvir <arunviratwal@gmail.com>
2024-10-22 11:14:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 st.title('EUR/USD Close Price Forecast using Theta Model')
 st.write("""This app forecasts EUR/USD close prices using the Theta model. You can upload your own dataset as a CSV file for analysis.""")
 
-df = pd.read_csv(`eurusd_24h.csv`)
+df = pd.read_csv('eurusd_24h.csv')
+st.dataframe(df.head())
