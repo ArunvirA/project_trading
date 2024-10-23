1f643acf8c22d0f366719903ca92548fb2e27f50
Arunvir <arunviratwal@gmail.com>
2024-10-23 09:25:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,10 @@ st.markdown(f"<p style='color: white; font-size: 18px;'>MAE: {mae_theta:.5f}</p>
 st.markdown(f"<p style='color: white; font-size: 18px;'>MSE: {mse_theta:.5f}</p>", unsafe_allow_html=True)
 st.markdown(f"<p style='color: white; font-size: 18px;'>RMSE: {rmse_theta:.5f}</p>", unsafe_allow_html=True)
 
-
+col1, col2, col3 = st.columns(3)
+col1.metric("MAE", "0.10251")
+col2.metric("MSE", "0.00837")
+col3.metric("RMSE", "0.09152")
 
 
 # def get_plotly_data():
@@ -81,17 +84,17 @@ st.markdown(f"<p style='color: white; font-size: 18px;'>RMSE: {rmse_theta:.5f}</
 #     x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
 #     return x, y, z
 # st.write(train_ts)
-st.write(train_ts.values())
-st.write(len(train_ts.values()))
+# st.write(train_ts.values())
+# st.write(len(train_ts.values()))
 
 # x, y, z = get_plotly_data()
-train_line = px.line(x=df_full.index[:len(train_ts.values())],y=train_ts.values())
-test_line = px.line(x=df_full.index[-len(test_ts.values())+1:],y=test_ts.values())
-forecast_line = px.line(x=df_full.index[-len(test_ts.values())+1:],y=forecast_values.values())
+# train_line = px.line(x=df_full.index[:len(train_ts.values())],y=train_ts.values())
+# test_line = px.line(x=df_full.index[-len(test_ts.values())+1:],y=test_ts.values())
+# forecast_line = px.line(x=df_full.index[-len(test_ts.values())+1:],y=forecast_values.values())
 
-fig = go.Figure(data=[train_line,test_line,forecast_line])
-fig.update_layout(title='EUR/USD predictions', autosize=False, width=800, height=800, margin=dict(l=40, r=40, b=40, t=40))
-st.plotly_chart(fig)
+# fig = go.Figure(data=[train_line,test_line,forecast_line])
+# fig.update_layout(title='EUR/USD predictions', autosize=False, width=800, height=800, margin=dict(l=40, r=40, b=40, t=40))
+# st.plotly_chart(fig)
 
 if st.button('More 🎈🎈🎈 please!'):
     st.balloons()
